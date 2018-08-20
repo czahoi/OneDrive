099f714e6c35387b8fd501434881d772e4b30bdb
iiiiiii1 <25471406+iiiiiii1@users.noreply.github.com>
2018-08-20 21:03:46 +0800

    Update OneDrive.sh

--- OneDrive.sh
+++ OneDrive.sh
@@ -2,12 +2,12 @@
 
 rm -rf /usr/local/etc/OneDrive
 mkdir -p /usr/local/etc/OneDrive
-wget --no-check-certificate -q -O /usr/local/etc/OneDrive/json-parser "https://raw.githubusercontent.com/lm317379829/OneDrive/master/Business/json-parser"
-wget --no-check-certificate -q -O /usr/local/etc/OneDrive/onedrive "https://raw.githubusercontent.com/lm317379829/OneDrive/master/Business/onedrive"
-wget --no-check-certificate -q -O /usr/local/etc/OneDrive/onedrive-d "https://raw.githubusercontent.com/lm317379829/OneDrive/master/Business/onedrive-d"
-wget --no-check-certificate -q -O /usr/local/etc/OneDrive/onedrive-authorize "https://raw.githubusercontent.com/lm317379829/OneDrive/master/Business/onedrive-authorize"
-wget --no-check-certificate -q -O /usr/local/etc/OneDrive/onedrive-base "https://raw.githubusercontent.com/lm317379829/OneDrive/master/Business/onedrive-base"
-wget --no-check-certificate -q -O /usr/local/etc/OneDrive/onedrive.cfg "https://raw.githubusercontent.com/lm317379829/OneDrive/master/Business/onedrive.cfg"
+wget --no-check-certificate -q -O /usr/local/etc/OneDrive/json-parser "https://raw.githubusercontent.com/iiiiiii1/OneDrive/master/Business/json-parser"
+wget --no-check-certificate -q -O /usr/local/etc/OneDrive/onedrive "https://raw.githubusercontent.com/iiiiiii1/OneDrive/master/Business/onedrive"
+wget --no-check-certificate -q -O /usr/local/etc/OneDrive/onedrive-d "https://raw.githubusercontent.com/iiiiiii1/OneDrive/master/Business/onedrive-d"
+wget --no-check-certificate -q -O /usr/local/etc/OneDrive/onedrive-authorize "https://raw.githubusercontent.com/iiiiiii1/OneDrive/master/Business/onedrive-authorize"
+wget --no-check-certificate -q -O /usr/local/etc/OneDrive/onedrive-base "https://raw.githubusercontent.com/iiiiiii1/OneDrive/master/Business/onedrive-base"
+wget --no-check-certificate -q -O /usr/local/etc/OneDrive/onedrive.cfg "https://raw.githubusercontent.com/iiiiiii1/OneDrive/master/Business/onedrive.cfg"
 chmod -R a+x /usr/local/etc/OneDrive
 ln -sf /usr/local/etc/OneDrive/onedrive /usr/local/bin/
 ln -sf /usr/local/etc/OneDrive/onedrive-d /usr/local/bin/
